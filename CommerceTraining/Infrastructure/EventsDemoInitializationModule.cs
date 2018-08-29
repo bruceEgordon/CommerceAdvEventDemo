9ca95cda4842f72bf1f93f97db6d4bbd76ab80a4
Bruce Gordon <bruce@focusls.com>
2018-08-29 18:35:23 -0400

    Add project files.

---
+++ CommerceTraining/Infrastructure/EventsDemoInitializationModule.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+using EPiServer;
+using EPiServer.Core;
+using EPiServer.Framework;
+using EPiServer.Framework.Initialization;
+using EPiServer.Logging;
+using EPiServer.ServiceLocation;
+
+namespace CommerceTraining.Infrastructure
+{
+    [InitializableModule]
+    [ModuleDependency(typeof(EPiServer.Web.InitializationModule), typeof(EPiServer.Commerce.Initialization.InitializationModule))]
+    public class EventsDemoInitializationModule : IInitializableModule
+    {
+        public void Initialize(InitializationEngine context)
+        {
+            var contentEv = context.Locate.ContentEvents();
+            contentEv.PublishedContent += ContentEv_PublishedContent;
+        }
+
+        private void ContentEv_PublishedContent(object sender, ContentEventArgs e)
+        {
+            var logger = LogManager.GetLogger();
+            logger.Error("Content Published Event!");
+            if (e.Content.GetOriginalType().Name == "ShirtVariation")
+            {
+                logger.Error($"{e.Content.GetOriginalType().Name}");
+            }
+        }
+
+        private void ContentEv_SavedContent(object sender, EPiServer.ContentEventArgs e)
+        {
+            var logger = LogManager.GetLogger();
+            logger.Error("Content Saved Event!");
+            if (e.Content.GetOriginalType().Name == "ShirtVariation")
+            {
+                logger.Error($"{e.Content.GetOriginalType().Name}");
+            }
+        }
+
+        public void Uninitialize(InitializationEngine context)
+        {
+            //Add uninitialization logic
+        }
+    }
+}
---
+++ EventDemoSite/Infrastructure/RemoteEventDemoInitializationModule.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using EPiServer.Events.Clients;
+using EPiServer.Framework;
+using EPiServer.Framework.Initialization;
+using EPiServer.Logging;
+using Mediachase.Commerce.Engine.Events;
+
+namespace EventDemoSite.Infrastructure
+{
+    [InitializableModule]
+    [ModuleDependency(typeof(EPiServer.Web.InitializationModule))]
+    public class RemoteEventDemoInitializationModule : IInitializableModule
+    {
+        Event evListener;
+        public void Initialize(InitializationEngine context)
+        {
+            evListener = Event.Get(CatalogKeyEventBroadcaster.CatalogKeyEventGuid);
+            evListener.Raised += EvListener_Raised;
+        }
+
+        private void EvListener_Raised(object sender, EPiServer.Events.EventNotificationEventArgs e)
+        {
+            var eventArgs = (CatalogKeyEventArgs)DeSerialize((byte[])e.Param);
+            var priceUpdatedEventArgs = eventArgs as PriceUpdateEventArgs;
+            var inventoryUpdatedEventArgs = eventArgs as InventoryUpdateEventArgs;
+            var logger = LogManager.GetLogger();
+            if(priceUpdatedEventArgs != null)
+            {
+                logger.Error($"the price was changed!");
+            }
+            if(inventoryUpdatedEventArgs != null)
+            {
+                logger.Error($"the inventory was changed!");
+            }
+        }
+
+        protected virtual CatalogKeyEventArgs DeSerialize(byte[] buffer)
+        {
+            var formatter = new BinaryFormatter();
+            var stream = new MemoryStream(buffer);
+            return formatter.Deserialize(stream) as CatalogKeyEventArgs;
+        }
+        public void Uninitialize(InitializationEngine context)
+        {
+            evListener.Raised -= EvListener_Raised;
+        }
+    }
+}
