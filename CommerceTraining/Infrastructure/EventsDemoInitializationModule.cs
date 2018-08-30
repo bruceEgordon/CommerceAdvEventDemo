211257c37c5f4958c16d690d8b6d77a5e90d10a1
Bruce Gordon <bruce@focusls.com>
2018-08-30 08:16:46 -0400

    1

--- CommerceTraining/Infrastructure/EventsDemoInitializationModule.cs
+++ CommerceTraining/Infrastructure/EventsDemoInitializationModule.cs
@@ -1,7 +1,10 @@
 ﻿using System;
+using System.IO;
 using System.Linq;
 using EPiServer;
+using EPiServer.Commerce.Catalog.Provider;
 using EPiServer.Core;
+using EPiServer.Events.Clients;
 using EPiServer.Framework;
 using EPiServer.Framework.Initialization;
 using EPiServer.Logging;
@@ -17,15 +20,33 @@ namespace CommerceTraining.Infrastructure
         {
             var contentEv = context.Locate.ContentEvents();
             contentEv.PublishedContent += ContentEv_PublishedContent;
+
+            var catEv = CatalogEventHandler.Instance;//ServiceLocator.Current.GetInstance<CatalogEventHandler>();
+
+            catEv.ContentUpdated += CatEv_ContentUpdated;
+
+        }
+
+        private void CatEv_ContentUpdated(object sender, ContentEventArgs e)
+        {
+            string mydocPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            using (StreamWriter ouputFile = new StreamWriter(Path.Combine(mydocPath, "EventsDemo.txt"), true))
+            {
+                ouputFile.WriteLine($"CatalogContentEvent fired for {e.Content.ToString()}");
+            }
         }
 
         private void ContentEv_PublishedContent(object sender, ContentEventArgs e)
         {
-            var logger = LogManager.GetLogger();
-            logger.Error("Content Published Event!");
+            string mydocPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+            using(StreamWriter ouputFile = new StreamWriter(Path.Combine(mydocPath, "EventsDemo.txt"), true))
+            {
+                ouputFile.WriteLine("Published content event fired!");
+            }
             if (e.Content.GetOriginalType().Name == "ShirtVariation")
             {
-                logger.Error($"{e.Content.GetOriginalType().Name}");
+                
             }
         }
 
